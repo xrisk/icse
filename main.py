33bf1e3005c92bfd647f148a250d6d26483f8743
Rishav Kundu <rishav.kundu98@gmail.com>
2015-08-12 03:51:37 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import BaseHTTPServer
+import sys, random, os, socket, urllib, json, urlparse, cgi
+from firebase import firebase
+
+
+
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+  
+  def do_GET(self):
+      print self.path
+      print self.headers 
+      self.send_response(200, 'OK')
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      
+      self.wfile.write('HELLO WORLD')
+        
+   
+  
+ 
+    
+    
+def main():
+  
+  if 'PORT' in os.environ:
+    HOST, PORT = socket.gethostname(), int(os.environ['PORT'])
+  else:
+    HOST, PORT = "localhost", random.choice(range(5000, 10000))
+  httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+  print "serving at port", PORT
+  httpd.serve_forever()
+  
+main()
