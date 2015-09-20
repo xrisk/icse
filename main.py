6995be4cc8738208be89bf4a23730346fab4663e
Rishav Kundu <rishav.kundu98@gmail.com>
2015-09-20 21:47:45 +0530

    v1

--- main.py
+++ main.py
@@ -1,58 +1,159 @@
-import BaseHTTPServer, base64, os, random
-from firebase import firebase
+import BaseHTTPServer
+import os
+import random
+import urlparse
+import pymongo
+from bs4 import BeautifulSoup as bs
+
+abbrev = {
+	"COMPUTER APPLICATIONS" : "CS",
+	"HISTORY CIVICS & GEOGRAPHY" : "HCG",
+	"MATHEMATICS" : "MATH"
+}
+
+def populate(json):
+	soup = bs(open('web/results.html').read())
+	headers = soup.find(id="headers")
+	data = soup.find(id="data")
+	print json
+	for i in ['Name', 'UID', 'School', 'Score']:
+		h  = soup.new_tag("th")
+		h.string = i
+		headers.append(h)
+
+	for i in ['name', '_id', 'school', 'best5']:
+		f = soup.new_tag("td")
+		f.string = json[i]
+		data.append(f)
 
 
-data = ''
-pwd = ''
+	for j in json['marks']:
+		h = soup.new_tag("th")
+		
+		if j in abbrev:
+			h.string = abbrev[j]
+		else:
+			h.string = j
+
+		headers.append(h)
+		h = soup.new_tag("td")
+		h.string = json['marks'][j]
+		data.append(h)
+
+
+	return soup.prettify()
+
+def populate_school(json):
+	subject_counts = {}
+	l = []
+	for i in json:
+		l.append(i)
+		for j in i['marks']:
+			if j in subject_counts:
+				subject_counts[j] += 1
+			else:
+				subject_counts[j] = 1
+	soup = bs(open('web/results.html').read())
+	headers = soup.find(id="headers")
+	data = soup.find(id="data")
+	print json
+	for i in ['Name', 'UID', 'Score']:
+		h  = soup.new_tag("th")
+		h.string = i
+		headers.append(h)
+	pairs = []
+	for i in subject_counts:
+		pairs.append( (subject_counts[i], i) )
+	pairs.sort(key = lambda x: (-x[0], x[1]))
+
+	for j in pairs:
+		h = soup.new_tag("th")
+		if j[1] in abbrev:
+			h.string = abbrev[j[1]]
+		else:
+			h.string = j[1]
+		headers.append(h)
+
+	print l
+	for i in l:
+		row = soup.new_tag("tr")
+		for j in ['name', '_id', 'best5']:
+			f = soup.new_tag("td")
+			f.string = i[j]
+			row.append(f)
+		for j in pairs:
+			f = soup.new_tag("td")
+			if j[1] in i["marks"]:
+				f.string = i["marks"][j[1]]
+			else:
+				f.string = ''
+			row.append(f)
+				
+		data.append(row)	
+	
+	return soup.prettify()
+	
+
+
+
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 	def do_GET(self):
-		print self.headers
+		if self.headers['X-Forwarded-Proto'] != 'https':
+			self.send_response(301, 'Must use SSL')
+			self.send_header('Location', 'https://icse.herokuapp.com' + self.path)
+			self.end_headers()
+		else:
+			path = self.path
+			if path == '/':
+				path = '/query.html'
+			path = 'web' + path
+			try:
+				resp = open(path).read()
+				self.send_response(200, 'OK')
+				self.end_headers()
+				self.wfile.write(resp)
+			except IOError:
+			  self.send_response(404, 'Not Found')
+			  self.end_headers()
+
+	def do_POST(self):
 		if self.headers['X-Forwarded-Proto'] != 'https':
 			self.send_response(301, 'Must use SSL')
 			self.send_header('Location', 'https://icse.herokuapp.com')
 			self.end_headers()
 		else:
-			if 'Authorization' not in self.headers:
-
-				self.send_response(401, 'Not authorized')
-				self.send_header('Content-type', 'text/html')
-				self.send_header('WWW-Authenticate', 'Basic realm="Enter a username and password"')
-				self.end_headers()
-			else:
-				try:
-					u = base64.b64decode(self.headers['Authorization'].split()[1])
-					print u
-					if u == pwd:
-						self.send_response(200, 'OK')
-						self.send_header('Content-type', 'text/html')
-						self.end_headers()
-						self.wfile.write(data)
-					else:
-						raise Exception
-				except Exception, e:
-					print e
-					self.send_response(401, 'Not authorized')
-					self.send_header('Content-type', 'text/html')
-					self.send_header('WWW-Authenticate', 'Basic realm="Enter a username and password"')
+			content = self.rfile.read(int(self.headers['Content-Length']))
+			content = urlparse.parse_qs(content)
+			print content
+			print self.password
+			if 'pass' in content and content['pass'][0] == os.environ['PASSWORD']:
+				if 'name' in content:
+					self.send_response(200, 'OK')
 					self.end_headers()
+					resp = self.db.icse.processed.find_one({"name":content['name'][0].upper()})
+					self.wfile.write(populate(resp))
+					
+				if 'school' in content:
+					print 'FIZZ'
+					self.send_response(200, 'OK')
+					self.end_headers()
+					resp = self.db.icse.processed.find({"school":content['school'][0].upper()}).sort("name")
+					self.wfile.write(populate_school(resp))
 
 
+
+
+			
+Handler.password = os.environ['PASSWORD']
+
+Handler.db = pymongo.MongoClient('mongodb://xrisk:{}@ds049631.mongolab.com:49631/icse'.format(os.environ['MONGO']))
 if 'PORT' in os.environ:
 	PORT = int(os.environ['PORT'])
 else:
 	PORT = random.choice(range(5000, 10000))
 
 httpd = BaseHTTPServer.HTTPServer(('', PORT), Handler)
-
-auth = firebase.FirebaseAuthentication(os.environ['FIREBASE_SECRET'],'icse:db', admin=True)
-db = firebase.FirebaseApplication('https://icse.firebaseio.com', authentication=auth)
-
-data = db.get('/data', '')
-pwd = db.get('/pwd', '')
-
-if pwd == '':
-	raise Exception
 
 print "serving at port", PORT
 
