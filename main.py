a59a4f9c82ad276be08dace5750762fa33551a8e
Rishav Kundu <rishav.kundu98@gmail.com>
2015-09-24 09:38:50 +0530

    Add caching support and clean up code.

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import BaseHTTPServer
-import os
-import random
-import urlparse
-import pymongo
-import sys
-import logging
-import tempfile
-import sha
 import gzip
+import logging
+import os
+import pymongo
+import random
+import sha
+import sys
+import tempfile
+import urlparse
 from bs4 import BeautifulSoup as bs
 
+def sha_hash(content):
+	return sha.new(content).hexdigest()
+
+def mime(path):
+	for i in mimetable:
+		if path.endswith(i):
+			return mimetable[i]
+	logging.warn('Mime type not found for' + path)
+	return 'text/plain'
+
 def gzip_content(content):
-	h = sha.new(content).hexdigest()
+	h = sha_hash(content) + '.gzip'
 	if os.path.isfile(h):
 		with open(h,'rb') as fin:
 			return fin.read()
@@ -21,6 +31,20 @@ def gzip_content(content):
 	with open(h, 'rb') as fin:
 		return fin.read()
 
+def has_gzip(self):
+	if 'Accept-Encoding' in self.headers and 'gzip' in self.headers['Accept-Encoding']:
+		return True
+	else:
+		return False
+
+def smart_reply(self, resp):
+	if self.has_gzip():
+		self.send_header('Content-Encoding', 'gzip')
+		self.end_headers()
+		self.wfile.write(gzip_content(resp))
+	else:
+		self.end_headers()
+		self.wfile.write(resp)
 
 abbrev = {
 	"COMPUTER APPLICATIONS" : "CS",
@@ -29,40 +53,21 @@ abbrev = {
 	"BIOLOGY" : "BIO"
 }
 
-mimetable = {'.html':'text/html', '.css':'text/css'}
+# let's be a nice compliant server
+mimetable = {
+	".css" : "text/css",
+	".eot" : "application/octet-stream",
+	".html" : "text/html",
+	".png" : "image/png",
+	".svg" : "image/svg+xml",
+	".ttf" : "application/x-font-ttf",
+	".woff" : "application/octet-stream",
+	".woff2" : "application/octet-stream",
+}
 
-def populate(json):
-	soup = bs(open('web/results.html').read())
-	headers = soup.find(id="headers")
-	data = soup.find(id="data")
-	for i in ['Name', 'UID', 'School', 'Score']:
-		h  = soup.new_tag("th")
-		h.string = i
-		headers.append(h)
+hash_lookup = {}
 
-	for i in ['name', '_id', 'school', 'best5']:
-		f = soup.new_tag("td")
-		f.string = json[i]
-		data.append(f)
-
-
-	for j in json['marks']:
-		h = soup.new_tag("th")
-		
-		if j in abbrev:
-			h.string = abbrev[j]
-		else:
-			h.string = j
-
-		headers.append(h)
-		h = soup.new_tag("td")
-		h.string = json['marks'][j]
-		data.append(h)
-
-
-	return soup.prettify()
-
-def populate_school(json):
+def populate(json, school=False):
 	subject_counts = {}
 	l = []
 	for i in json:
@@ -72,10 +77,13 @@ def populate_school(json):
 				subject_counts[j] += 1
 			else:
 				subject_counts[j] = 1
-	soup = bs(open('web/results.html').read())
+	soup = bs(open('web/results.html').read(), 'lxml')
 	headers = soup.find(id="headers")
 	data = soup.find(id="data")
-	for i in ['Name', 'UID', 'Score']:
+	meta = ['Name', 'UID', 'Score']
+	if school:
+		meta.append('School')
+	for i in meta:
 		h  = soup.new_tag("th")
 		h.string = i
 		headers.append(h)
@@ -91,10 +99,12 @@ def populate_school(json):
 		else:
 			h.string = j[1]
 		headers.append(h)
-
+	meta = ['name', '_id', 'best5']
+	if school:
+		meta.append('school') 
 	for i in l:
 		row = soup.new_tag("tr")
-		for j in ['name', '_id', 'best5']:
+		for j in meta:
 			f = soup.new_tag("td")
 			f.string = i[j]
 			row.append(f)
@@ -108,10 +118,8 @@ def populate_school(json):
 				
 		data.append(row)	
 	
-	return soup.prettify()
+	return soup.prettify().encode('utf-8')
 	
-
-
 
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -120,24 +128,46 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			self.send_response(301, 'Must use SSL')
 			self.send_header('Location', 'https://icse.herokuapp.com' + self.path)
 			self.end_headers()
+			return 
+		
+		if self.path == '/':
+			path = os.path.join('web', 'query.html')
 		else:
-			path = self.path
-			if path == '/':
-				path = '/query.html'
-			path = 'web' + path
-			try:
-				resp = open(path).read()
-				self.send_response(200, 'OK')
-				if 'Accept-Encoding' in self.headers and 'gzip' in self.headers['Accept-Encoding']:
-						self.send_header('Content-Encoding', 'gzip')
-						self.end_headers()
-						self.wfile.write(gzip_content(resp))
-				else:
-					self.end_headers()
-					self.wfile.write(resp)
-			except IOError:
-			  self.send_response(404, 'Not Found')
-			  self.end_headers()
+			if self.path.startswith('/'):
+				path = os.path.join('web', self.path[1:])
+			else:
+				path = os.path.join('web', self.path)
+		
+		if not os.path.isfile(path):
+			self.send_response(404)
+			self.send_header('Content-Type', 'text/html')
+			with open('web/404-generic.html') as fin:
+				resp = fin.read()
+			self.write(resp)
+			return
+
+		if path in hash_lookup:
+			h = hash_lookup[path]
+
+		if 'If-None-Match' in self.headers:
+			if h == self.headers['If-None-Match']:
+				self.send_response(304, "Unchanged")
+				self.send_header("Cache-Control", "no-cache, max-age=604800")
+				self.send_header("ETag", h)
+				self.end_headers()
+				return
+
+		with open(path) as fin:
+			resp = fin.read()
+
+		h = sha_hash(resp)
+		hash_lookup[path] = h
+
+		self.send_response(200, 'OK')
+		self.send_header("Cache-Control", "no-cache, max-age=604800")
+		self.send_header("Content-type", mime(path))
+		self.send_header("ETag", h)
+		self.write(resp)
 
 	def do_POST(self):
 		if '--force-ssl' in sys.argv and self.headers['X-Forwarded-Proto'] != 'https':
@@ -145,50 +175,57 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			self.send_response('Content')
 			self.send_header('Location', 'https://icse.herokuapp.com')
 			self.end_headers()
+			return
+
+		content = self.rfile.read(int(self.headers['Content-Length']))
+		content = urlparse.parse_qs(content)
+		
+		if not ('pass' in content) and (('school' in content) ^ ('name' in content)):
+			
+			# since a browser wont generate a malformed POST,
+			# this must be someone using cURL or something similar
+			# dont serve error page
+
+			self.send_response(400, "Bad POST request.")
+			self.end_headers()
+			return
+
+		if content['pass'][0] not in self.password:
+			self.send_response(403, 'Forbidden')
+			self.send_header('Content-Type', "text/html")
+			with open('web/403.html') as fin:
+				resp = fin.read()
+			self.write(resp)
+			return
+			
+		if 'name' in content:
+			resp = self.db.icse.processed.find({"name":content['name'][0].upper()})
+		elif 'school' in content:
+			resp = self.db.icse.processed.find({"school":content['school'][0].upper()}).sort("name")
 		else:
-			content = self.rfile.read(int(self.headers['Content-Length']))
-			content = urlparse.parse_qs(content)
-			if 'pass' in content and content['pass'][0] == os.environ['PASSWORD']:
-				if 'name' in content:
-					self.send_response(200, 'OK')
-					self.send_header('Content-type', 'text/html')
-					resp = self.db.icse.processed.find_one({"name":content['name'][0].upper()})
-					if resp is None:
-						self.end_headers()
-						self.wfile.write(open('web/404.html').read())
-					else:
-						if 'Accept-Encoding' in self.headers and 'gzip' in self.headers['Accept-Encoding']:
-							self.send_header('Content-Encoding', 'gzip')
-							self.end_headers()
-							self.wfile.write(gzip_content(populate(resp).encode('utf-8')))
-						else:
-							self.wfile.write(populate(resp).encode('utf-8'))
-					
-				if 'school' in content:
-					self.send_response(200, 'OK')
-					self.send_header('Content-type', 'text/html')
-					resp = self.db.icse.processed.find({"school":content['school'][0].upper()}).sort("name")
-					if resp.count() == 0:
-						self.end_headers()
-						self.wfile.write(open('web/404-school.html').read())
-					else:
-						if 'Accept-Encoding' in self.headers and 'gzip' in self.headers['Accept-Encoding']:
-							self.send_header('Content-Encoding', 'gzip')
-							self.end_headers()
-							self.wfile.write(gzip_content(populate_school(resp).encode('utf-8')))
-						else:
-							self.wfile.write(populate_school(resp).encode('utf-8'))
-			else:
-				self.send_response(403, 'Not authorised')
-				self.send_header('Content-type', 'text/html')
-				self.end_headers()
-				self.wfile.write(open('web/403.html').read())
-
-
-
-
+			logging.critical('Malformed POST.')
 	
-Handler.password = os.environ['PASSWORD']
+		if resp.count() == 0:
+			self.send_response(404)
+			if 'name' in content:
+				with open('web/404-name.html') as fin:
+					resp = fin.read()
+			else:
+				with open('web/404-school.html') as fin:
+					resp = fin.read()
+			self.send_header('Content-type', 'text/html')
+			self.write(resp)
+			return
+
+		resp = populate(resp, school=('name' in content))
+		self.send_response(200, 'OK')
+		self.send_header('Content-type','text/html')
+		self.send_header('Cache-Control', 'no-store')
+		self.write(resp)
+
+Handler.password = [os.environ['PASSWORD']]
+Handler.has_gzip = has_gzip
+Handler.write = smart_reply
 
 Handler.db = pymongo.MongoClient('mongodb://xrisk:{}@ds049631.mongolab.com:49631/icse'.format(os.environ['MONGO']))
 if 'PORT' in os.environ:
