24610667ac3fccdbbf3f6de28a2918aac95bf734
Rishav Kundu <rishav.kundu98@gmail.com>
2015-09-26 18:17:02 +0530

    flake8 style checks.

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import pymongo
 import random
 import sha
 import sys
-import tempfile
 import urlparse
 from bs4 import BeautifulSoup as bs
 
+
 def sha_hash(content):
 	return sha.new(content).hexdigest()
+
 
 def mime(path):
 	for i in mimetable:
@@ -20,10 +21,11 @@ def mime(path):
 	logging.warn('Mime type not found for' + path)
 	return 'text/plain'
 
+
 def gzip_content(content):
 	h = sha_hash(content) + '.gzip'
 	if os.path.isfile(h):
-		with open(h,'rb') as fin:
+		with open(h, 'rb') as fin:
 			return fin.read()
 	with gzip.open(h, 'wb') as fout:
 		fout.write(content)
@@ -31,11 +33,14 @@ def gzip_content(content):
 	with open(h, 'rb') as fin:
 		return fin.read()
 
+
 def has_gzip(self):
-	if 'Accept-Encoding' in self.headers and 'gzip' in self.headers['Accept-Encoding']:
-		return True
+	if 'Accept-Encoding' in self.headers:
+		if  'gzip' in self.headers['Accept-Encoding']:
+			return True
 	else:
 		return False
+
 
 def smart_reply(self, resp):
 	if self.has_gzip():
@@ -47,25 +52,26 @@ def smart_reply(self, resp):
 		self.wfile.write(resp)
 
 abbrev = {
-	"COMPUTER APPLICATIONS" : "CS",
-	"HISTORY CIVICS & GEOGRAPHY" : "HCG",
-	"MATHEMATICS" : "MATH",
-	"BIOLOGY" : "BIO"
+	"COMPUTER APPLICATIONS": "CS",
+	"HISTORY CIVICS & GEOGRAPHY": "HCG",
+	"MATHEMATICS": "MATH",
+	"BIOLOGY": "BIO"
 }
 
 # let's be a nice compliant server
 mimetable = {
-	".css" : "text/css",
-	".eot" : "application/octet-stream",
-	".html" : "text/html",
-	".png" : "image/png",
-	".svg" : "image/svg+xml",
-	".ttf" : "application/x-font-ttf",
-	".woff" : "application/octet-stream",
-	".woff2" : "application/octet-stream",
+	".css": "text/css",
+	".eot": "application/octet-stream",
+	".html": "text/html",
+	".png": "image/png",
+	".svg": "image/svg+xml",
+	".ttf": "application/x-font-ttf",
+	".woff": "application/octet-stream",
+	".woff2": "application/octet-stream",
 }
 
 hash_lookup = {}
+
 
 def populate(json, school=False):
 	subject_counts = {}
@@ -84,13 +90,13 @@ def populate(json, school=False):
 	if school:
 		meta.append('School')
 	for i in meta:
-		h  = soup.new_tag("th")
+		h = soup.new_tag("th")
 		h.string = i
 		headers.append(h)
 	pairs = []
 	for i in subject_counts:
-		pairs.append( (subject_counts[i], i) )
-	pairs.sort(key = lambda x: (-x[0], x[1]))
+		pairs.append((subject_counts[i], i))
+	pairs.sort(key=lambda x: (-x[0], x[1]))
 
 	for j in pairs:
 		h = soup.new_tag("th")
@@ -101,7 +107,7 @@ def populate(json, school=False):
 		headers.append(h)
 	meta = ['name', '_id', 'best5']
 	if school:
-		meta.append('school') 
+		meta.append('school')
 	for i in l:
 		row = soup.new_tag("tr")
 		for j in meta:
@@ -114,22 +120,23 @@ def populate(json, school=False):
 				f.string = i["marks"][j[1]]
 			else:
 				f.string = ''
+
 			row.append(f)
-				
-		data.append(row)	
-	
+
+		data.append(row)
+
 	return soup.prettify().encode('utf-8')
-	
 
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 	def do_GET(self):
-		if '--force-ssl' in sys.argv and self.headers['X-Forwarded-Proto'] != 'https':
-			self.send_response(301, 'Must use SSL')
-			self.send_header('Location', 'https://icse.herokuapp.com' + self.path)
-			self.end_headers()
-			return 
-		
+		if '--force-ssl' in sys.argv:
+			if self.headers['X-Forwarded-Proto'] != 'https':
+				self.send_response(301, 'Must use SSL')
+				self.send_header('Location', 'https://icse.herokuapp.com' + self.path)
+				self.end_headers()
+				return
+
 		if self.path == '/':
 			path = os.path.join('web', 'query.html')
 		else:
@@ -137,7 +144,7 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 				path = os.path.join('web', self.path[1:])
 			else:
 				path = os.path.join('web', self.path)
-		
+
 		if not os.path.isfile(path):
 			self.send_response(404)
 			self.send_header('Content-Type', 'text/html')
@@ -170,18 +177,19 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 		self.write(resp)
 
 	def do_POST(self):
-		if '--force-ssl' in sys.argv and self.headers['X-Forwarded-Proto'] != 'https':
-			self.send_response(301, 'Must use SSL')
-			self.send_response('Content')
-			self.send_header('Location', 'https://icse.herokuapp.com')
-			self.end_headers()
-			return
+		if '--force-ssl' in sys.argv:
+			if self.headers['X-Forwarded-Proto'] != 'https':
+				self.send_response(301, 'Must use SSL')
+				self.send_response('Content')
+				self.send_header('Location', 'https://icse.herokuapp.com')
+				self.end_headers()
+				return
 
 		content = self.rfile.read(int(self.headers['Content-Length']))
 		content = urlparse.parse_qs(content)
-		
+
 		if not ('pass' in content) and (('school' in content) ^ ('name' in content)):
-			
+
 			# since a browser wont generate a malformed POST,
 			# this must be someone using cURL or something similar
 			# dont serve error page
@@ -197,14 +205,15 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 				resp = fin.read()
 			self.write(resp)
 			return
-			
+
 		if 'name' in content:
-			resp = self.db.icse.processed.find({"name":content['name'][0].upper()})
+			resp = self.db.icse.processed.find({"name": content['name'][0].upper()})
 		elif 'school' in content:
-			resp = self.db.icse.processed.find({"school":content['school'][0].upper()}).sort("name")
+			resp = self.db.icse.processed.find(
+				{"school": content['school'][0].upper()}).sort("name")
 		else:
 			logging.critical('Malformed POST.')
-	
+
 		if resp.count() == 0:
 			self.send_response(404)
 			if 'name' in content:
@@ -219,7 +228,7 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 		resp = populate(resp, school=('name' in content))
 		self.send_response(200, 'OK')
-		self.send_header('Content-type','text/html')
+		self.send_header('Content-type', 'text/html')
 		self.send_header('Cache-Control', 'no-store')
 		self.write(resp)
 
@@ -227,7 +236,9 @@ Handler.password = [os.environ['PASSWORD']]
 Handler.has_gzip = has_gzip
 Handler.write = smart_reply
 
-Handler.db = pymongo.MongoClient('mongodb://xrisk:{}@ds049631.mongolab.com:49631/icse'.format(os.environ['MONGO']))
+Handler.db = pymongo.MongoClient(
+	'mongodb://xrisk:{}@ds049631.mongolab.com:49631/icse'
+	.format(os.environ['MONGO']))
 if 'PORT' in os.environ:
 	PORT = int(os.environ['PORT'])
 else:
@@ -238,4 +249,3 @@ httpd = BaseHTTPServer.HTTPServer(('', PORT), Handler)
 print "serving at port", PORT
 
 httpd.serve_forever()
-
